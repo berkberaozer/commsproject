b3694493d8ff9903304e630b6e0a647ab7a897b1
Berk Bera Özer <berkberaozer@gmail.com>
2024-07-08 12:21:03 +0300

    Consumers turned to async, added dependencies to the daphne.

--- base/consumers.py
+++ base/consumers.py
@@ -1,5 +1,6 @@
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.db import database_sync_to_async
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 
@@ -10,114 +11,145 @@ import redis
 r = redis.Redis(host='localhost', port=6379)
 
 
-class ChatConsumer(WebsocketConsumer):
+class ChatConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
         self.chat_id = None
 
-    def connect(self):
+    async def connect(self):
         self.chat_id = self.scope["url_route"]["kwargs"]["chat_id"]
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.chat_id, self.channel_name
-        )
+        await self.channel_layer.group_add(self.chat_id, self.channel_name)
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.chat_id, self.channel_name
-        )
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.chat_id, self.channel_name)
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         data = json.loads(text_data)
-        date = timezone.now()
         message_type = data["type"]
 
         if message_type == "chat_message":
-            message = Message.objects.create(source=get_user_model().objects.get(id=data["source_id"]),
-                                             message=data["message"], date=date, chat=Chat.objects.get(id=self.chat_id))
-            message.save()
+            message = await self.create_message(data["source_id"], data["message"], self.chat_id)
 
-            async_to_sync(self.channel_layer.group_send)(self.chat_id,
-                                                         {"type": "chat_message", "source_id": message.source_id,
-                                                          "message": message.message,
-                                                          "date": date.__str__(),
-                                                          "message_id": message.id, "hasReached": message.hasReached,
-                                                          "hasRead": message.hasRead, "chat_id":
-                                                              message.chat_id, "hasSent": message.hasSent}
-                                                         )
+            await self.channel_layer.group_send(self.chat_id, {"type": "chat_message", "source_id": message.source_id,
+                                                               "message": message.message,
+                                                               "date": message.date.__str__(),
+                                                               "message_id": message.id,
+                                                               "hasReached": message.hasReached,
+                                                               "hasRead": message.hasRead, "chat_id":
+                                                                   message.chat_id, "hasSent": message.hasSent}
+                                                )
         elif message_type == "message_reached":
-            msg = Message.objects.get(id=data["message_id"])
-            msg.hasReached = True
-            msg.save()
+            await self.update_reached(data["message_id"], True)
 
-            async_to_sync(self.channel_layer.group_send)(self.chat_id,
-                                                         {"type": message_type, "message_id": data["message_id"]}
-                                                         )
+            await self.channel_layer.group_send(self.chat_id, {"type": message_type, "message_id": data["message_id"]})
         elif message_type == "message_read":
-            msg = Message.objects.get(id=data["message_id"])
-            msg.hasRead = True
-            msg.save()
+            await self.update_read(data["message_id"], True)
 
-            async_to_sync(self.channel_layer.group_send)(self.chat_id,
-                                                         {"type": message_type, "message_id": data["message_id"]}
-                                                         )
+            await self.channel_layer.group_send(self.chat_id, {"type": message_type, "message_id": data["message_id"]})
 
-    def chat_message(self, event):
-        self.send(text_data=json.dumps(event))
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps(event))
 
-    def message_read(self, event):
-        self.send(text_data=json.dumps(event))
+    async def message_read(self, event):
+        await self.send(text_data=json.dumps(event))
 
-    def message_reached(self, event):
-        self.send(text_data=json.dumps(event))
+    async def message_reached(self, event):
+        await self.send(text_data=json.dumps(event))
+
+    @database_sync_to_async
+    def create_message(self, source_id, message, chat_id):
+        date = timezone.now()
+        message = Message.objects.create(source=get_user_model().objects.get(id=source_id),
+                                         message=message, date=date, chat=Chat.objects.get(id=self.chat_id))
+        message.save()
+
+        return message
+
+    @database_sync_to_async
+    def update_read(self, message_id, boolean):
+        msg = Message.objects.get(id=message_id)
+        msg.hasRead = boolean
+        msg.save()
+
+        return msg
+
+    @database_sync_to_async
+    def update_reached(self, message_id, boolean):
+        msg = Message.objects.get(id=message_id)
+        msg.hasReached = boolean
+        msg.save()
+
+        return msg
 
 
-class UserConsumer(WebsocketConsumer):
+class UserConsumer(AsyncWebsocketConsumer):
 
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
         self.username = None
 
-    def connect(self):
+    async def connect(self):
         self.username = self.scope["url_route"]["kwargs"]["username"]
-        self.accept()
+
+        await self.accept()
+
+        await self.channel_layer.group_add(self.username, self.channel_name)
 
         if self.username == self.scope["user"].username:
-            get_user_model().objects.filter(username=self.username).update(online=True)
+            await self.update_online(True)
 
-            async_to_sync(self.channel_layer.group_send)(self.username, {"type": "is_online", "value": True})
+            await self.channel_layer.group_send(self.username, {"type": "is_online", "value": True})
 
-        async_to_sync(self.channel_layer.group_add)(self.username, self.channel_name)
-
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         if self.username == self.scope["user"].username:
-            get_user_model().objects.filter(username=self.username).update(online=False)
+            await self.update_online(False)
+            await self.channel_layer.group_send(self.username, {"type": "is_online", "value": False})
 
-            async_to_sync(self.channel_layer.group_send)(self.username, {"type": "is_online", "value": False})
+        await self.channel_layer.group_discard(self.username, self.channel_name)
 
-        async_to_sync(self.channel_layer.group_discard)(self.username, self.channel_name)
-
-    def receive(self, text_data):
+    async def receive(self, text_data):
         data = json.loads(text_data)
-        message_type = data["type"]
 
-        if message_type == "chat_creation" or message_type == "chat_creation_ack":
-            async_to_sync(self.channel_layer.group_send)(self.username, {"type": message_type,
-                                                                         "source_username": data["source_username"],
-                                                                         "chat_id": data["chat_id"]})
-        elif message_type == "is_online":
-            async_to_sync(self.channel_layer.group_send)(self.username, {"type": message_type,
-                                                                         "value": list(get_user_model().objects.filter
-                                                                                       (username=self.username).values(
-                                                                             'online'))[0]["online"]})
+        if data["type"] == "chat_creation":
+            source_username = data["source_username"]
+            chat_id = data["chat_id"]
+            await self.channel_layer.group_send(
+                self.username, {"type": "chat_creation", "source_username": source_username, "chat_id": chat_id}
+            )
+        elif data["type"] == "chat_creation_ack":
+            source_username = data["source_username"]
+            chat_id = data["chat_id"]
+            await self.channel_layer.group_send(
+                self.username, {"type": "chat_creation_ack", "chat_id": chat_id, "source_username": source_username}
+            )
+        elif data["type"] == "is_online":
+            await self.channel_layer.group_send(
+                self.username, {"type": "is_online", "value": await self.get_online()}
+            )
 
-    def chat_creation(self, event):
-        self.send(text_data=json.dumps(event))
+    async def chat_creation(self, event):
+        source_username = event["source_username"]
+        chat_id = event["chat_id"]
+        message_type = event["type"]
 
-    def chat_creation_ack(self, event):
-        self.send(text_data=json.dumps(event))
+        await self.send(
+            text_data=json.dumps({"type": message_type, "source_username": source_username, "chat_id": chat_id})
+        )
 
-    def is_online(self, event):
-        self.send(text_data=json.dumps(event))
+    async def chat_creation_ack(self, event):
+        await self.send(text_data=json.dumps(
+            {"type": event["type"], "chat_id": event["chat_id"], "source_username": event["source_username"]}))
+
+    async def is_online(self, event):
+        await self.send(text_data=json.dumps({"type": event["type"], "value": event["value"]}))
+
+    @database_sync_to_async
+    def update_online(self, boolean):
+        return get_user_model().objects.filter(username=self.username).update(online=boolean)
+
+    @database_sync_to_async
+    def get_online(self):
+        return list(get_user_model().objects.filter(username=self.username).values('online'))[0]["online"]
